f5caa0435959c8dfd5ac69181c983de503bb084d
Yaya <42711252+ayagaidi@users.noreply.github.com>
2026-09-09 00:12:47 +0200

    style: format scanner exceptions

--- app/Exceptions/MrzNotDetectedException.php
+++ app/Exceptions/MrzNotDetectedException.php
@@ -4,6 +4,4 @@ namespace App\Exceptions;
 
 use RuntimeException;
 
-final class MrzNotDetectedException extends RuntimeException
-{
-}
+final class MrzNotDetectedException extends RuntimeException {}
