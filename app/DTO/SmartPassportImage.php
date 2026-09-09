da3fdd78fa8fe85b51ec6aee01a9d3b644fee9de
Yaya <42711252+ayagaidi@users.noreply.github.com>
2026-09-09 07:34:07 +0200

    feat: add smart scanner image DTO

---
+++ app/DTO/SmartPassportImage.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\DTO;
+
+final readonly class SmartPassportImage
+{
+    public function __construct(
+        public string $mrzImagePath,
+        public string $visualZoneImagePath,
+        public array $temporaryPaths = [],
+        public string $strategy = 'full_image_fallback',
+        public array $preprocessing = [],
+    ) {}
+}
